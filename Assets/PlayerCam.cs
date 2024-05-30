ef177961533752fc085a7586de3bb2acabb9cf14
Hong Lam To <raymond0586@msn.com>
2024-05-29 22:59:07 -0400

    Step1 updated

---
+++ Assets/PlayerCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX; // Sensitivity for mouse X axis
+    public float sensY; // Sensitivity for mouse Y axis
+
+    public Transform orientation; // Reference to the player's orientation transform
+
+    float xRotation; // Current rotation around the X axis
+    float yRotation; // Current rotation around the Y axis
+
+    private void Start()
+    {
+        // Lock the cursor in the middle of the screen and make it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        // Update rotation values based on mouse input
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        // Clamp the rotation around the X axis to prevent looking up or down more than 90 degrees
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate the camera and player orientation along both the X and Y axes
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        
+        // Rotate the player's orientation along the Y axis
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
